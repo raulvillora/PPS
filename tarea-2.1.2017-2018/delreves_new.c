125952fd9fadf35bf932adb8c6a5529ad751da08
Unknown <pablo.pg.94@gmail.com>
2017-12-29 17:58:36 +0100

    Probando unas cosillas con un archivo de prueba

--- tarea-2.1.2017-2018/delreves_new.c
+++ tarea-2.1.2017-2018/delreves_new.c
@@ -5,9 +5,24 @@
 #include "auxiliar.h"
 
 
-#define length 3000
+const int LENGTH = 3000;
 
 
 int main(int argc, char **argv) {
- char cadena(length);
+ char cadena[LENGTH];
+ int i = 0;
+ FILE *AEntrada = NULL;
+ if(argc == 1) {
+  while (fgets(cadena,LENGTH,stdin) != NULL) {
+   int j;
+   for(j = LENGTH - 2; j >= 0; j--) {
+    printf("%c", cadena[j]);
+   }
+   printf("\n");
+  }
+  return 0;
+ }
+ if(strcmp(argv[1], "-h") == 0 || (strcmp(argv[1], "--help") == 0)) {
+  printf("delreves: Uso:")
+ }
 }
