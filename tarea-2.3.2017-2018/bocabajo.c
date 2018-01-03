ed1e7f42f2c28f23ec9270af8e882490c90a4f1c
Raul Villora Valencia <raulwalencia@gmail.com>
2018-01-03 16:32:00 +0100

    Comentado archivo bocabajo.c. Falta revisar los comentarios.

--- tarea-2.3.2017-2018/bocabajo.c
+++ tarea-2.3.2017-2018/bocabajo.c
@@ -4,7 +4,15 @@
 #include <sysexits.h>
 #include "auxiliar.h"
 
+//Usamos el prefijo const para declarar la constante LENGTH con un tamaño máximo de 2048
 #define TAM_CAD 2048
+
+/**
+ * Recibe dos parámetros de entrada: argc y argv.
+ * Estos dos son conocidos como argumentos para la línea de comandos.
+ * argc hace referencia al número de argumentos que le estamos pasando al programa.
+ * argv es un array de punteros, en el que cada elemento que tiene dentro apunta a uno de los argumentos que le hemos pasado.
+*/
 
 int main(int argc, char **argv)
 {
@@ -14,38 +22,49 @@ int main(int argc, char **argv)
 	int contador = 0;
 	int i = 0;
 	int aux = 0;
-	char cadena[TAM_CAD];
+	char cadena[TAM_CAD]; //El array cadena va a poder almacenar como máximo 2048 carácteres por línea
 	FILE *AEntrada = NULL;
 	if (argc == 1)
 	{
+		//malloc(size_t size) asgina la memoria solicitada y le devuelve un puntero. Con ello creamos el puntero pprimer del array y comprobamos que no sea nulo
 		if ((pprimer = (char **)malloc(sizeof(char *))) == NULL)
 		{
 			argv0 = "bocabajo";
 			Error(EX_OSERR, "%s", "No se  pudo ubicar la memoria dinamica necesaria.");
 		}
+		//fgets lee el contenido del fichero stdin (nombre introducido como argumento) y lo copia en cadena
+		//Esta función sólo lee 2047 caracteres
+		//Mientras sean distintas de NULL se va leyendo
 		while (fgets(cadena, TAM_CAD, stdin) != NULL)
 		{
+			//Mediante la llamada a la función realloc() se puede incrementar el tamaño donde esta asginado el bloque de memoria permitiendo que el puntero de la línea siguiente entre en nuestro array
 			pprimer = (char **)realloc(pprimer, ((contador + 1) * sizeof(char *)));
-			if ((psegun = (char *)strdup(cadena)) == NULL)
+			if ((psegun = (char *)strdup(cadena)) == NULL) // Devuelve un puntero a una cadena de bytes terminados en nulo, que es un duplicado de la cadena apuntada por strdup(cadena). Se comprueba que no es nulo. El puntero devuelto se debe pasar a libre para evitar una pérdida de memoria. Si se produce un error, se devuelve un puntero nulo y se puede establecer un errno.
 			{
 				argv0 = "bocabajo";
 				Error(EX_OSERR, "%s", "No se  pudo ubicar la memoria dinamica necesaria.");
 			}
 			else
 			{
-				*(pprimer + contador) = psegun;
+				*(pprimer + contador) = psegun; //En este caso guardaremos el puntero, psegun, en la posición siguiente del array
 			}
 			contador++;
 		}
-		free(pprimer[contador]);
-		for (j = contador - 1; j >= 0; j--)
+		free(pprimer[contador]);			//Mediante el uso de la función free(void *address) liberamos un bloque de memoria especificado por la dirección que es el puntero de la ultima linea leida al ser un salto de linea no deseado
+		for (j = contador - 1; j >= 0; j--) //Recorremos el array de punteros
 		{
-			printf("%s", pprimer[j]);
-			free(pprimer[j]);
+			printf("%s", pprimer[j]); //Imprimimos cada caracter contenido en el array pprimer desde el final al principio de éste
+			free(pprimer[j]);		  //Una vez impresa esa posición la liberamos
 		}
-		free(pprimer);
+		free(pprimer); //Se libera el array completo
 		return 0;
 	}
+	/*
+	* La función strcmp(s1, s2) devuelve los siguientes valores dependiendo de los argumentos que se le pasen:
+	* 0 si s1 y s2 son iguales
+	* menor que 0 si s1 < s2
+	* mayor que 0 si s1 > s2
+	*/
 	if (strcmp(argv[1], "-h") == 0 || (strcmp(argv[1], "--help") == 0))
 	{
 		printf("bocabajo: .*Uso: .*bocabajo.c...El programa recibe un fichero o cadena de texto,\n");
@@ -62,27 +81,30 @@ int main(int argc, char **argv)
 		for (i = 1; i <= argc - 1; i++)
 		{
 			AEntrada = NULL;
-			AEntrada = fopen(argv[i], "r");
+			AEntrada = fopen(argv[i], "r"); //Leemos el archivo
+			//Si no existe lanzamos un error
 			if (AEntrada == NULL)
 			{
 				argv0 = "bocabajo";
 				Error(EX_NOINPUT, ".*\"%s\"", argv[i], "no legible");
-				/*continue; Aunque el fichero no sea valido continuamos en el for*/
 			}
 			else
 			{
-				while (fgets(cadena, TAM_CAD, AEntrada) != NULL) /*Leemos las lines*/
+				//fgets lee el contenido del fichero stdin (nombre introducido como argumento) y lo copia en cadena
+				//Esta función sólo lee 2047 caracteres
+				//Mientras sean distintas de NULL se va leyendo
+				while (fgets(cadena, TAM_CAD, AEntrada) != NULL)
 				{
 					aux = 1;
 					if (contador == 0) /*primer caso*/
 					{
-
-						if ((pprimer = (char **)malloc(sizeof(char *))) == NULL) /*Creamos puntero del array de punteros,y comprobamos que no sea nulo*/
+						//malloc(size_t size) asgina la memoria solicitada y le devuelve un puntero. Con ello creamos el puntero pprimer del array y comprobamos que no sea nulo
+						if ((pprimer = (char **)malloc(sizeof(char *))) == NULL)
 						{
 							argv0 = "bocabajo";
 							Error(EX_OSERR, "%s", "No se  pudo ubicar la memoria dinamica necesaria.");
 						}
-						if ((psegun = (char *)strdup(cadena)) == NULL) /*Duplicamos la linea leida en memoria dinamica y comprobamos que el puntero a dicha linea no sea nulo*/
+						if ((psegun = (char *)strdup(cadena)) == NULL) // Devuelve un puntero a una cadena de bytes terminados en nulo, que es un duplicado de la cadena apuntada por strdup(cadena). Se comprueba que no es nulo. El puntero devuelto se debe pasar a libre para evitar una pérdida de memoria. Si se produce un error, se devuelve un puntero nulo y se puede establecer un errno.
 						{
 							argv0 = "bocabajo";
 							Error(EX_OSERR, "%s", "No se  pudo ubicar la memoria dinamica necesaria.");
@@ -93,9 +115,9 @@ int main(int argc, char **argv)
 						}
 					}
 					else
-					{
-						pprimer = (char **)realloc(pprimer, ((contador + 1) * sizeof(char *))); /*Aumentamos el tamaño del array de punteros para que entre el puntero de la linea siguiente*/
-						if ((psegun = (char *)strdup(cadena)) == NULL)							/*/*Duplicamos la linea leida en memoria dinamica y comprobamos que el puntero a dicha linea no sea nulo*/
+					{ //Mediante la llamada a la función realloc() se puede incrementar el tamaño donde esta asginado el bloque de memoria permitiendo que el puntero de la línea siguiente entre en nuestro array
+						pprimer = (char **)realloc(pprimer, ((contador + 1) * sizeof(char *)));
+						if ((psegun = (char *)strdup(cadena)) == NULL) // Devuelve un puntero a una cadena de bytes terminados en nulo, que es un duplicado de la cadena apuntada por strdup(cadena). Se comprueba que no es nulo. El puntero devuelto se debe pasar a libre para evitar una pérdida de memoria. Si se produce un error, se devuelve un puntero nulo y se puede establecer un errno.
 						{
 							argv0 = "bocabajo";
 							Error(EX_OSERR, "%s", "No se  pudo ubicar la memoria dinamica necesaria.");
@@ -108,17 +130,16 @@ int main(int argc, char **argv)
 					contador++;
 				}
 			}
-			/*free(pprimer[contador]);Liberamos el puntero de la ultima linea leida al ser un salto de linea no deseado*/
 			fclose(AEntrada);
 		}
 		if (aux != 0)
 		{
-			for (j = contador - 1; j >= 0; j--) /*Bucle para recorrer el array de  punteros*/
+			for (j = contador - 1; j >= 0; j--) //Recorremos el array de punteros
 			{
-				printf("%s", pprimer[j]); /*Impresion lineas caracter a caracter*/
-				free(pprimer[j]);		  /*Liberar array de  punteros*/
+				printf("%s", pprimer[j]); //Imprimimos cada caracter contenido en el array pprimer desde el final al principio de éste
+				free(pprimer[j]);		  //Una vez impresa esa posición la liberamos
 			}
-			free(pprimer); /*Liberar array de  punteros*/
+			free(pprimer); //Se libera el array completo
 		}
 	}
 	return 0;
